6f73f4e6b20a88c694e3ec764e4c96fcd9d79c84
Jeyder30 <jeydernicolay@gmail.com>
2026-05-20 17:41:11 -0500

    arreglos

--- src/main/java/com/aibert/dosw/domain/port/out/NotificationEventPort.java
+++
@@ -1,8 +0,0 @@
-package com.aibert.dosw.domain.port.out;
-
-import com.aibert.dosw.domain.model.NotificationEvent;
-
-public interface NotificationEventPort {
-
-    void sendNotification(NotificationEvent event);
-}
