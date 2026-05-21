7488b9fa949965d0ba61d981af6a789f539858f6
Joshua <joshua.quiroga-l@mail.escuelaing.edu.co>
2026-05-21 00:55:20 -0500

    [feature]: Correccion de hotspots de seguridad HTTP para cumplir rating A en SonarCloud

--- src/main/java/com/aibert/dosw/config/OpenApiConfig.java
+++ src/main/java/com/aibert/dosw/config/OpenApiConfig.java
@@ -3,18 +3,11 @@ package com.aibert.dosw.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
 public class OpenApiConfig {
-
-        @Value("${server.port:1505}")
-        private int serverPort;
 
         @Bean
         public OpenAPI openApi() {
@@ -34,10 +27,6 @@ public class OpenApiConfig {
                                                 .version("v1.0")
                                                 .contact(new Contact()
                                                                 .name("AI-BERT Backend Team")
-                                                                .url("https://github.com/AI-BERT-BACKEND")))
-                                .servers(List.of(
-                                                new Server()
-                                                                .url("http://localhost:" + serverPort)
-                                                                .description("Local Development")));
+                                                                .url("https://github.com/AI-BERT-BACKEND")));
         }
 }
